de99e5215bc97ee9399a52901a9a729015e3f2c7
Jason Mortensen <ingrey1@gmail.com>
2022-07-31 01:54:01 -0700

    update-generate-hash-algorithm

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * Importing the Block class
  */
 //
-const BlockClass = require('./block.js');
+const BlockClass = require("./block.js");
 
 /**
  * Creating a block object
@@ -10,15 +10,15 @@ const BlockClass = require('./block.js');
 const block = new BlockClass.Block("Test Block");
 
 // Generating the block hash
-block.generateHash().then((result) => {
-	console.log(`Block Hash: ${result.hash}`);
-	console.log(`Block: ${JSON.stringify(result)}`);
-}).catch((error) => {console.log(error)});
 
-/**
- * Step 3: Run the application in node.js
- * 
- */
+const generateBlockHash = async () => {
+  try {
+    const blockHash = await block.generateHash();
+    console.log(`Block Hash: ${blockHash}`);
+    console.log(`Block: ${JSON.stringify(block)}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-// From the terminal: cd into Project folder
-// From the terminal: Run node app.js to run the code
+generateBlockHash();
